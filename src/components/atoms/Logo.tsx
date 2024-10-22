09e3befc006d317a24a957b5434417eaecb97c18
vector55764 <vectorforwork@gmail.com>
2024-10-22 14:35:02 +0300

    feat(atoms): Add Logo component

---
+++ src/components/atoms/Logo.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+
+const SLogo = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+  color: #000;
+  letter-spacing: 1px;
+`
+
+export const Logo = () => {
+  return <SLogo>Exclusive</SLogo>
+}
